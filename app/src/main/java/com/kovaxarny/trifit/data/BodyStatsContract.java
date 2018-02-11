cecd693e7f7580a4fb9787b1301f47922f17ac8b
kovaxarny <kovaxarny96@gmail.com>
2018-02-11 15:22:16 +0100

    Creating contract class

---
+++ app/src/main/java/com/kovaxarny/trifit/data/BodyStatsContract.java
@@ -0,0 +1,17 @@
+package com.kovaxarny.trifit.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by kovax on 2018-02-11.
+ */
+
+public class BodyStatsContract {
+
+    public static final class BodyStatsEntry implements BaseColumns{
+        public static final String TABLE_NAME = "bodystats";
+        public static final String COLUMN_HEIGHT = "height";
+        public static final String COLUMN_WEIGHT = "weight";
+        public static final String COLUMN_DATE = "date";
+    }
+}
