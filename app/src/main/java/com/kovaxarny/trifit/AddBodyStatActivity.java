c4a0a139643686b3c001c1bb20ecabf4c3545a94
kovaxarny <kovaxarny96@gmail.com>
2018-02-13 19:25:47 +0100

    Datepicker added to addBodyStat activity

--- app/src/main/java/com/kovaxarny/trifit/AddBodyStatActivity.java
+++ app/src/main/java/com/kovaxarny/trifit/AddBodyStatActivity.java
@@ -1,19 +1,31 @@
 package com.kovaxarny.trifit;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class AddBodyStatActivity extends AppCompatActivity {
     private static final String TAG = "AddBodyStatActivity";
 
     private EditText etAddHeight;
     private EditText etAddWeight;
+    private EditText etAddDate;
     private Button addButton;
+
+    private int year_x,month_x,day_x;
+    static final int DIALOG_ID = 0;
+    private final Calendar calendar = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +40,24 @@ public class AddBodyStatActivity extends AppCompatActivity {
         addButton.setOnClickListener(addButtonListener);
 
         etAddHeight.requestFocus();
+
+        year_x = calendar.get(Calendar.YEAR);
+        month_x = calendar.get(Calendar.MONTH);
+        day_x = calendar.get(Calendar.DAY_OF_MONTH);
+
+        etAddDate = (EditText) findViewById(R.id.et_new_date);
+        updateEditText();
+        showDialogOnEditTextClick();
     }
 
     View.OnClickListener addButtonListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             Intent output = new Intent();
-            if (!etAddHeight.getText().toString().equals("") && !etAddWeight.getText().toString().equals("")){
+            if (!etAddHeight.getText().toString().equals("") && !etAddWeight.getText().toString().equals("") && !etAddDate.getText().toString().equals("")){
                 output.putExtra("addWeight", etAddHeight.getText().toString());
                 output.putExtra("addHeight", etAddWeight.getText().toString());
+                output.putExtra("addDate", etAddDate.getText().toString());
                 setResult(RESULT_OK, output);
                 finish();
             }else{
@@ -44,4 +65,38 @@ public class AddBodyStatActivity extends AppCompatActivity {
             }
         }
     };
+
+    private void showDialogOnEditTextClick(){
+        etAddDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(DIALOG_ID);
+            }
+        });
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id){
+        if (id == DIALOG_ID)
+            return new DatePickerDialog(this, datePickerListener, year_x ,month_x ,day_x);
+        else
+            return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, monthOfYear);
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateEditText();
+        }
+    };
+
+    private void updateEditText(){
+        String myFormat = "yyyy/MM/dd"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        etAddDate.setText(sdf.format(calendar.getTime()));
+    }
 }
