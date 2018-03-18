242bdc894d20766c554d3b1dd3fae9273a5c9ee2
kovaxarny <kovaxarny96@gmail.com>
2018-03-18 13:01:47 +0100

    Notification Action added

--- app/src/main/java/com/kovaxarny/trifit/utilities/NotificationUtil.java
+++ app/src/main/java/com/kovaxarny/trifit/utilities/NotificationUtil.java
@@ -1,5 +1,6 @@
 package com.kovaxarny.trifit.utilities;
 
+import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -22,10 +23,13 @@ import com.kovaxarny.trifit.drawer.WorkoutProgramsActivity;
 public class NotificationUtil {
 
     private static final int WORKOUT_REMINDER_NOTIFICATION_ID = 1138;
-
     private static final int WORKOUT_REMINDER_PENDING_INTENT_ID = 3417;
 
+    private static final int ACTION_IGNORE_PENDING_INTENT_ID = 14;
+
     private static final String WORKOUT_REMINDER_NOTIFICATION_CHANNEL_ID = "reminder_notification_channel";
+    private static final String ACTION_DISMISS_NOTIFICATION = "dismiss-notification";
+
 
     public static void remindUserToWorkout(Context context) {
         NotificationManager notificationManager = (NotificationManager)
@@ -50,6 +54,7 @@ public class NotificationUtil {
                                 context.getString(R.string.workout_reminder_notification_body)))
                         .setDefaults(Notification.DEFAULT_VIBRATE)
                         .setContentIntent(contentIntent(context))
+                        .addAction(ignoreReminderAction(context))
                         .setAutoCancel(true);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
@@ -58,6 +63,26 @@ public class NotificationUtil {
         }
 
         notificationManager.notify(WORKOUT_REMINDER_NOTIFICATION_ID, notificationBuilder.build());
+    }
+
+    private static NotificationCompat.Action ignoreReminderAction(Context context) {
+        Intent ignoreReminderIntent = new Intent(context, WorkoutReminderIntentService.class);
+        ignoreReminderIntent.setAction(ACTION_DISMISS_NOTIFICATION);
+        PendingIntent ignoreReminderPendingIntent = PendingIntent.getService(
+                context,
+                ACTION_IGNORE_PENDING_INTENT_ID,
+                ignoreReminderIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Action ignoreReminderAction = new NotificationCompat.Action(R.drawable.ic_cancel,
+                "No, thanks.",
+                ignoreReminderPendingIntent);
+        return ignoreReminderAction;
+    }
+
+    public static void clearAllNotifications(Context context) {
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancelAll();
     }
 
     private static PendingIntent contentIntent(Context context) {
@@ -72,5 +97,28 @@ public class NotificationUtil {
 
     private static Bitmap largeIcon(Context context) {
         return BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_directions_run_black_24dp);
+    }
+
+    private static void executeTask(Context context, String action) {
+        if (ACTION_DISMISS_NOTIFICATION.equals(action)) {
+            clearAllNotifications(context);
+        }
+    }
+
+    public static class WorkoutReminderIntentService extends IntentService {
+
+        public WorkoutReminderIntentService(String name) {
+            super(name);
+        }
+
+        public WorkoutReminderIntentService() {
+            super("Workout ReminderIntentService");
+        }
+
+        @Override
+        protected void onHandleIntent(Intent intent) {
+            String action = intent.getAction();
+            executeTask(this, action);
+        }
     }
 }
