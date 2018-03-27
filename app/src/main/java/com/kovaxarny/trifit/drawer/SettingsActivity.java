00b1722f3029885aa018bc7cde294a95da64c295
kovaxarny <kovaxarny96@gmail.com>
2018-03-27 20:13:44 +0200

    Created Setting activity UI

--- app/src/main/java/com/kovaxarny/trifit/drawer/SettingsActivity.java
+++ app/src/main/java/com/kovaxarny/trifit/drawer/SettingsActivity.java
@@ -3,12 +3,8 @@ package com.kovaxarny.trifit.drawer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
 
 import com.kovaxarny.trifit.R;
-import com.kovaxarny.trifit.notifications.ReminderUtilities;
-import com.kovaxarny.trifit.utilities.NotificationUtil;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -17,32 +13,9 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        Button testNotificationButton = (Button) findViewById(R.id.button_Test_Notification);
-        Button cancelNotificationButton = (Button) findViewById(R.id.cancel_Test_Notification);
-
-        testNotificationButton.setOnClickListener(testNotificationListener);
-        cancelNotificationButton.setOnClickListener(cancelTestNotificationListener);
-
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
-    View.OnClickListener testNotificationListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-//            ReminderUtilities.scheduleWorkoutReminder(getApplicationContext());
-            NotificationUtil.remindUserToLog(getApplicationContext());
-        }
-    };
-
-    View.OnClickListener cancelTestNotificationListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            ReminderUtilities.cancelWorkoutReminder(getApplicationContext());
-        }
-    };
-
 }
