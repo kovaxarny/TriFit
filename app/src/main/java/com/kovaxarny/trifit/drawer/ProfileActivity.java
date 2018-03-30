74586e1bd4172adb63aa64b05bd27abb955cd7ea
kovaxarny <kovaxarny96@gmail.com>
2018-03-30 15:46:25 +0200

    Created Profile Layout

--- app/src/main/java/com/kovaxarny/trifit/drawer/ProfileActivity.java
+++ app/src/main/java/com/kovaxarny/trifit/drawer/ProfileActivity.java
@@ -1,10 +1,8 @@
 package com.kovaxarny.trifit.drawer;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.TextView;
 
 import com.kovaxarny.trifit.R;
 
@@ -14,15 +12,6 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
-        TextView mDisplayText = (TextView) findViewById(R.id.tv_profile_activity);
-
-        Intent callerIntent = getIntent();
-
-        if (callerIntent.hasExtra("Text")) {
-            mDisplayText.setText(callerIntent.getStringExtra("Text"));
-        }
-
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
